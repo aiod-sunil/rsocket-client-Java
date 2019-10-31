a6d563a1b9dd5e489206ef9f5173bfa5bbd21de1
Sunil Yadav <aiodsunil@gmail.com>
2019-10-31 21:03:45 +0000

    fixed issue

--- src/main/java/com/example/client/RsocketClientJavaApplication.java
+++ src/main/java/com/example/client/RsocketClientJavaApplication.java
@@ -5,6 +5,7 @@ import io.rsocket.RSocketFactory;
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import lombok.RequiredArgsConstructor;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -25,30 +27,35 @@ public class RsocketClientJavaApplication {
         SpringApplication.run(RsocketClientJavaApplication.class, args);
     }
 
-
-
-}
-
-@Configuration
-class RSocketConfig {
-
-
 	@Bean
-	RSocketRequester requester(RSocketStrategies rSocketStrategies) {
-		return RSocketRequester.builder(this.rSocket(), MimeTypeUtils.APPLICATION_JSON, rSocketStrategies);
-	}
-
-	@Bean
-	RSocket rSocket() {
-		return RSocketFactory
-				.connect()
-				.frameDecoder(PayloadDecoder.ZERO_COPY)
-				.dataMimeType(MimeTypeUtils.APPLICATION_JSON_VALUE)
-				.transport(TcpClientTransport.create(7000))
-				.start()
+	RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
+		return RSocketRequester.builder()
+				.rsocketStrategies(rSocketStrategies)
+				.connectTcp("localhost", 7000)
 				.block();
 	}
+
 }
+
+//@Configuration
+// class ClientConfiguration {
+//
+//	@Bean
+//	public RSocket rSocket() {
+//		return RSocketFactory
+//				.connect()
+//				.mimeType(MimeTypeUtils.APPLICATION_JSON_VALUE, MimeTypeUtils.APPLICATION_JSON_VALUE)
+//				.frameDecoder(PayloadDecoder.ZERO_COPY)
+//				.transport(TcpClientTransport.create(7000))
+//				.start()
+//				.block();
+//	}
+
+//	@Bean
+//	RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
+//		return RSocketRequester.wrap(rSocket(), MimeTypeUtils.APPLICATION_JSON,MimeTypeUtils.APPLICATION_JSON, rSocketStrategies);
+//	}
+//}
 @RestController
 class GreetingController {
 	private final RSocketRequester requester;
@@ -57,8 +64,85 @@ class GreetingController {
 		this.requester = requester;
 	}
 
-	@GetMapping(value = "/hi")
-	Mono<Void> hello() {
-		return this.requester.route("hello").data("hello").send();
+	@GetMapping(value = "/hello")
+	Publisher<Void> hello() {
+		return requester.
+				route("hello")
+				.data(new Person("Sunil"))
+				.send();
+	}
+	@GetMapping("/greet/{name}")
+	public Publisher<GreetingsResponse> greet(@PathVariable String name) {
+		return requester
+				.route("greet")
+				.data(new GreetingsRequest(name))
+				.retrieveMono(GreetingsResponse.class);
+	}
+}
+
+class Person{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Person(String name) {
+		this.name = name;
+	}
+	public Person(){
+
+	}
+}
+
+class GreetingsRequest {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public GreetingsRequest(String name) {
+		this.name = name;
+	}
+	public GreetingsRequest() {
+
+	}
+}
+
+
+
+class GreetingsResponse {
+
+	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public GreetingsResponse(String greeting) {
+		this.greeting = greeting;
+	}
+	public GreetingsResponse(){
+
 	}
 }
